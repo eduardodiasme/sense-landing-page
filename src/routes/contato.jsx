ae12e8e4f6869944254cb0820b169a850be53b55
Eduardo <cediasfl@gmail.com>
2023-12-12 19:03:22 -0300

    'contato' page boxes added

--- src/routes/contato.jsx
+++ src/routes/contato.jsx
@@ -1,8 +1,17 @@
 export default function Contato() {
-    return (
-      <>
-          Contato
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <div>
+        <form>
+          <h3>Seu nome</h3>
+          <input type="text" placeholder=" " />
+          <h3> Seu e-mail</h3>
+          <input type="email" placeholder="" />
+          <h3>Fale um pouco da sua empresa</h3>
+          <input type="text" placeholder="" className="comentario" />
+          <input type="button" value="Enviar" className="button" />
+        </form>
+      </div>
+    </>
+  );
+}
