fb5d0caa453f4b30a3edf2f697a8fd4b857fa29a
eduardodiasme <cediasfl@gmail.com>
2023-12-13 01:12:46 -0300

    'home' added

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,6 +1,16 @@
 export default function Home() {
   return (
     <>
+      <div className="center-container">
+        <div className="container-text">
+          <h1>
+            Cuide do seu negócio, <br />
+            deixe a tecnologia a Sense.
+          </h1>
+          <button>Entrar em contato</button>
+        </div>
+        <img src="/public/sam.png" />
+      </div>
     </>
   );
 }
